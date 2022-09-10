d281721e6a0fe8a51c291f4cd4447d2f924445d9
Ji Seungmin <cswcsm02@gmail.com>
2022-09-11 00:28:47 +0900

    data app

--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -1,13 +1,53 @@
 package com.example.test1
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import java.util.*
 
-//fragment는 이동이 Activity간 이루어지는 것이 아니라 한 화면 즉 한 Activiry에서 이동이 일어남
-//네비게이션 만들고 fragment들 만들어야 함
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startBtn = findViewById<Button>(R.id.start)
+        val endBtn = findViewById<Button>(R.id.end)
+
+        var startDate = ""
+        var endDate = ""
+
+        startBtn.setOnClickListener{
+
+            val today = GregorianCalendar()
+            val year = today.get(Calendar.YEAR)
+            val month = today.get(Calendar.MONTH)
+            val day = today.get(Calendar.DATE)
+
+            val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener{
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, date: Int) {
+                    startDate = year.toString() + (month+1).toString() + date.toString()
+
+                }
+            }, year, month, day )
+            dlg.show()
+        }
+        endBtn.setOnClickListener {
+            val today = GregorianCalendar()
+            val year = today.get(Calendar.YEAR)
+            val month = today.get(Calendar.MONTH)
+            val day = today.get(Calendar.DATE)
+
+            val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener{
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, date: Int) {
+                    endDate = year.toString() + (month+1).toString() + date.toString()
+
+                    findViewById<TextView>(R.id.between).setText((endDate.toInt()-startDate.toInt()).toString())
+                }
+            }, year, month, day )
+            dlg.show()
+        }
     }
 }
